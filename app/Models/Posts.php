e6b03e7b727e60e980a519a22d2a49cb838c8c9c
Michael Angelo De Leon <mad@marlons-iMac.local>
2020-09-27 02:32:34 +0800

    posts model

---
+++ app/Models/Posts.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class Posts extends Model
+{
+    use HasFactory, HasRoles, Notifiable;
+
+    protected $fillable = [
+        'user_id',
+        'post_content',
+        'post_privacy',
+        'post_feelings',
+        'post_location',
+        'active',
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+}
