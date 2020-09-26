c8fdab2258b0a559a842d2969468adc9685d6950
Michael Angelo De Leon <mad@marlons-iMac.local>
2020-09-26 18:38:46 +0800

    Spatie provider added

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Added Providers
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
