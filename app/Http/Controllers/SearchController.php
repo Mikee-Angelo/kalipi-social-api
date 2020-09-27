843de83a0b1078066499c25ea23acc316fe5688b
Michael Angelo De Leon <mad@marlons-iMac.local>
2020-09-27 15:13:45 +0800

    search controller

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class Searches extends Controller
+{
+    //
+    public function get($slug, Request $request){
+        $data = User::where([
+            ['name', 'like', '%'.$slug.'%'],
+            ['id', '!=', 1]
+        ])->get();
+    }
+}
