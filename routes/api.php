264d1a6bdeed79b0b0fac7b2a4d666d7521db056
Michael Angelo De Leon <mad@marlons-iMac.local>
2020-09-27 12:54:32 +0800

    comments added

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CommentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,10 +32,17 @@ Route::group([
     Route::post('refresh', [ApiAuthController::class, 'refresh']);
     Route::post('me', [ApiAuthController::class, 'me']);
 
-    // POSTS
+    // POST
     Route::post('/posts/create', [PostsController::class, 'create']);
-    Route::post('/posts/{posts_id}/destroy', [PostsController::class, 'destroy']);
+    Route::post('/posts/{posts_id}', [PostsController::class, 'getSinglePost']);
     Route::post('/posts/{posts_id}/update', [PostsController::class, 'update']);
-    Route::post('/posts/{posts_id}', [PostsController::class, 'singlePosts']);
+    Route::post('/posts/{posts_id}/destroy', [PostsController::class, 'destroy']);
+
+    //COMMENT
+    Route::post('/comments/{posts_id}/create', [CommentsController::class, 'create']);
+    Route::post('/comments/{comments_id}', [CommentsController::class, 'getSingleComment']);
+    Route::post('/comments/{comments_id}/update', [CommentsController::class, 'updates']);
+    Route::post('/comments/{comments_id}/destroy', [CommentsController::class, 'destroy']);
+    
 
 });
